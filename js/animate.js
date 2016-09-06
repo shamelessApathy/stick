73fd83ed9c170305cdf8de37a0aaf14c07b6e6a2
Brian Moniz <brian@prohoods.com>
2016-09-06 12:47:09 -0600

    Fixed a problem with the borderCheck() function where once one if/else was satisfied it would just return the value and not check if any other borders were in danger of being crossed!

--- js/animate.js
+++ js/animate.js
@@ -7,41 +7,99 @@ var Animate = function(){
 		var outerWidth = $(element.sprite).outerWidth();
 		var bottomY = el.y + outerHeight;
 		
-		if (el.y + outerHeight >= 300)
+		var downTest = function(){
+			if (el.y + outerHeight >= 300)
+			{
+				return true;
+			}
+			else {
+				return false;
+			}
+		};
+		var upTest = function(){
+			if( el.y <= -10 )
+			{
+				console.log('up test returning true');
+				return true;
+			}
+			else {
+				return false;
+			}
+		};
+		var leftTest = function(){
+			if( el.x <= -6 )
+			{
+				return true;
+			}
+			else {
+				return false;
+			}
+		};
+		var rightTest = function(){
+			if( el.x  + outerWidth >= 555 )
+			{
+				return true;
+			}
+			else {
+				return false;
+			}
+		};
+		if (downTest() && leftTest())
 		{
-			return 'down';
+			return ['down','left'];
 		}
-		else if( el.y <= -10 )
+		else if (upTest() && leftTest())
 		{
-			return 'up';
+			return ['up','left'];
 		}
-		else if( el.x <= -6 )
+		else if (rightTest() && upTest())
 		{
-			return 'left';
+			return ['up','right'];
 		}
-		else if( el.x  + outerWidth >= 555 )
+		else if (rightTest() && downTest())
 		{
-			return 'right';
+			return ['down','right'];
+		}
+		else if (downTest())
+		{
+			return ['down'];
+		}
+		else if (upTest())
+		{
+			console.log('uptest firing');
+			return ['up'];
+		}
+		else if (leftTest())
+		{
+			return ['left'];
+		}
+		else if (rightTest())
+		{
+			return ['right'];
+		}
+		else
+		{
+			return [];
 		}
 	};	
 	this.figure = function(){
 	
-		if(bpad.isDown('up') === 0 && this.borderCheck(stickFigure) !== 'up')
+		if(bpad.isDown('up') === 0 && this.borderCheck(stickFigure).indexOf('up') === -1)
 		{
 			stickFigure.y -= 2;
 			stickFigure.draw();
 		}
-		if(bpad.isDown('down') === 0 && this.borderCheck(stickFigure) !== 'down')
+		if(bpad.isDown('down') === 0 && this.borderCheck(stickFigure).indexOf('down') === -1)
 		{
 			stickFigure.y += 2;
 			stickFigure.draw();
 		}
-		if(bpad.isDown('left') === 0 && this.borderCheck(stickFigure) !== 'left')
+		if(bpad.isDown('left') === 0 && this.borderCheck(stickFigure).indexOf('left') === -1)
 		{
 			stickFigure.x -= 2;
 			stickFigure.draw();
 		}
-		if(bpad.isDown('right') === 0 && this.borderCheck(stickFigure) !== 'right')
+		if(bpad.isDown('right') === 0 && this.borderCheck(stickFigure).indexOf('right') === -1)
 		{
 			stickFigure.x += 2;
 			stickFigure.draw();
